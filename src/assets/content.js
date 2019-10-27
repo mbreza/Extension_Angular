8501e7720c21bd844afa9eb03cd50d306cfdaa9f
Mateusz Breza <breza.mateusz@gmail.com>
2019-10-27 14:03:41 +0100

    Changing manifest.json

--- src/assets/content.js
+++ src/assets/content.js
@@ -1,22 +1,18 @@
 window.addEventListener("message", function (event) {
   if (event.source == window && event.origin == "https://localhost:8443") {
     if (event.data.type == "encrypt") {
-      encrypt({sender: '', messageType: ''})
+      encrypt();
     } else if (event.data.type == "currentSignIn") {
       currentSignIn();
     } else if (event.data.type == "decrypt") {
-      decrypt({sender: '', messageType: ''})
-    } else if (event.data.type == "encryptStorage") {
-      encrypt({sender: event.data.sender, messageType: event.data.messageType})
-    } else if (event.data.type == "decryptStorage") {
-      decrypt({sender: event.data.sender, messageType: event.data.messageType})
+      decrypt(event.data.sender);
     } else {
       // console.log('JESTEM W ELSE');
     }
   }
 });
 
-function decrypt(value) {
+function decrypt(sender) {
   let arrayByte = event.data.key.substring(1);
   arrayByte = arrayByte.substring(0, arrayByte.length - 1);
   arrayByte = arrayByte.split(",")
@@ -35,27 +31,16 @@ function decrypt(value) {
       alert('You need to sign in to addon.')
     } else {
        console.log('Wiadmosc rozszyfrowana w content.js: ' + res.decryptedMessage);
-      if (value.sender !== '') {
-        // console.log('storage');
-        // console.log(value.sender);
-        window.postMessage({
-          type: 'decryptedStorage'.toString(),
-          message: res.decryptedMessage,
-          sender: value.sender,
-          messageType: value.messageType
-        }, "https://localhost:8443");
-      } else {
-        // console.log('not storage decrypt');
         window.postMessage({
           type: 'decrypted'.toString(),
-          message: res.decryptedMessage
+          message: res.decryptedMessage,
+          sender: sender
         }, "https://localhost:8443");
-      }
     }
   });
 }
 
-function encrypt(value) {
+function encrypt() {
   let arrayByte = event.data.key.substring(1);
   arrayByte = arrayByte.substring(0, arrayByte.length - 1);
   arrayByte = arrayByte.split(",")
@@ -72,23 +57,10 @@ function encrypt(value) {
     if (res.encryptedMessage == 'Not logged in!') {
       alert('You need to sign in to addon.')
     } else {
-      // console.log(res.encryptedMessage);
-      if (value.sender !== '') {
-        // console.log('storage');
-        // console.log(value.sender);
-        window.postMessage({
-          type: 'encryptedStorage'.toString(),
-          encyptedMessage: res.encryptedMessage.replace(/^\s+|\s+$/g, ''),
-          sender: value.sender,
-          messageType: value.messageType
-        }, "https://localhost:8443")
-      } else {
-        // console.log('not storage encrypt');
         window.postMessage({
           type: 'encrypted'.toString(),
           encyptedMessage: res.encryptedMessage.replace(/^\s+|\s+$/g, '')
         }, "https://localhost:8443")
-      }
     }
   });
 }
